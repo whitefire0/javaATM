c9ae20af699a85ce6241071b988607270c58a689
whitefire0 <rickhallett@live.co.uk>
2018-07-22 14:38:48 +0100

    class main to test application

--- src/com/whitefire0/Main.java
+++ src/com/whitefire0/Main.java
@@ -3,6 +3,7 @@ package com.whitefire0;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ATM theATM = new ATM();
+        theATM.run();
     }
 }
